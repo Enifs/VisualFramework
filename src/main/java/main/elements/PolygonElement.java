d6c7a3b4276285aca77552780b22352a753e95ae
BONNe1704 <bonne@bonne.id.lv>
2016-02-17 13:04:14 +0200

    Add method that resets all points, so PolygonElement center are located into (0, 0) coordinate.
    Comment all methods.

--- src/main/java/main/elements/PolygonElement.java
+++ src/main/java/main/elements/PolygonElement.java
@@ -1,12 +1,3 @@
-//
-//  Armands Vitols
-//  Didzis Romanovskis
-//  Ojars Vilmars Ratnieks
-//
-//  Manage 2015 under GPLv3
-//
-
-
 package main.elements;
 
 
@@ -33,18 +24,37 @@ public class PolygonElement extends Element
 	}
 
 
+	/**
+	 * This constructor creates new Polygon element from given pointList. Default background color is white.
+	 *
+	 * @param pointList List that contains all polygon points.
+	 */
 	public PolygonElement(List<Point2D> pointList)
 	{
 		this(pointList, Color.WHITE);
 	}
 
 
+	/**
+	 * This constructor creates new Polygon element from given pointList with given background color.
+	 *
+	 * @param pointList List that contains all polygon points.
+	 * @param backgroundColor Background color.
+	 */
 	public PolygonElement(List<Point2D> pointList, Color backgroundColor)
 	{
 		this(pointList, backgroundColor, Color.BLACK);
 	}
 
 
+	/**
+	 * This constructor creates new Polygon element from given pointList with given background and border
+	 * color.
+	 *
+	 * @param pointList List that contains all polygon points.
+	 * @param backgroundColor Background color.
+	 * @param borderColor Border color.
+	 */
 	public PolygonElement(List<Point2D> pointList, Color backgroundColor, Color borderColor)
 	{
 		if (pointList.size() < 3)
@@ -61,6 +71,28 @@ public class PolygonElement extends Element
 	}
 
 
+	/**
+	 * This method resets all points coordinates, so mass center for polygon is in (0, 0) coordinate.
+	 */
+	public void normalizePointCoordinates()
+	{
+		double x = this.polygon.getBounds2D().getCenterX();
+		double y = this.polygon.getBounds2D().getCenterY();
+
+		List<Point2D> newPointList = new ArrayList<>(this.pointList.size());
+
+		for (Point2D point : this.pointList)
+		{
+			newPointList.add(new Point2D(point.x() - x, point.y() - y));
+		}
+
+		this.pointList = newPointList;
+	}
+
+
+	/**
+	 * This method create Polygon element from pointList.
+	 */
 	public void buildPolygon()
 	{
 		this.polygon = new Polygon();
@@ -74,6 +106,9 @@ public class PolygonElement extends Element
 	}
 
 
+	/**
+	 * Object drawing method.
+	 */
 	@Override
 	public void draw(Graphics2D g)
 	{
@@ -94,12 +129,17 @@ public class PolygonElement extends Element
 // --------------------------------------------------------------------
 
 
+	/**
+	 * Method that returns polygon bounds.
+	 *
+	 * @return Bounds as Rectangle2D element.
+	 */
 	@Override
 	public Rectangle2D getBounds()
 	{
 		return new Rectangle2D(
-			this.getPosition().x(),
-			this.getPosition().y(),
+			this.polygon.getBounds2D().getMinX(),
+			this.polygon.getBounds2D().getMinY(),
 			this.polygon.getBounds2D().getWidth(),
 			this.polygon.getBounds2D().getHeight());
 	}
@@ -110,18 +150,33 @@ public class PolygonElement extends Element
 // ---------------------------------------------------------------------
 
 
+	/**
+	 * This method sets polygon background.
+	 *
+	 * @param color Background color.
+	 */
 	public void setBackgroundColor(Color color)
 	{
 		this.backgroundColor = color;
 	}
 
 
+	/**
+	 * This method sets Border color.
+	 *
+	 * @param color Border color.
+	 */
 	public void setBorderColor(Color color)
 	{
 		this.borderColor = color;
 	}
 
 
+	/**
+	 * This method sets polygon position.
+	 *
+	 * @param position Polygon position.
+	 */
 	@Override
 	public void setPosition(Point2D position)
 	{
